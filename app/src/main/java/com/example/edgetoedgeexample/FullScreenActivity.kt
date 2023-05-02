3680938a99526a3fb443abaca7c54c9c1d5f1d1b
John Codeos <johncodeos@gmail.com>
2023-05-02 11:43:46 +0300

    Improved code and bumped SDK version from 15 to 21

--- app/src/main/java/com/example/edgetoedgeexample/FullScreenActivity.kt
+++ app/src/main/java/com/example/edgetoedgeexample/FullScreenActivity.kt
@@ -1,10 +1,10 @@
 package com.example.edgetoedgeexample
 
-import android.content.ContentResolver
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.github.chrisbanes.photoview.PhotoView
 
@@ -20,22 +20,29 @@ class FullScreenActivity : AppCompatActivity() {
         // Photo Credits: https://unsplash.com/photos/G85VuTpw6jg
         fullScreenPhotoView.setImageResource(R.drawable.airplane)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (isGestureNavigationEnabled(this.contentResolver)) {
-                // Extends the PhotoView in the whole screen
-                window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-                // Hides StatusBar and Navigation bar, you have to tap to appear
-                // window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                // Fixes the Full Screen black bar in screen with notch
-                window.attributes.layoutInDisplayCutoutMode =
-                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
-            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && isGestureNavigationEnabled()) {
+            configureFullScreenMode()
         }
     }
 
-    private fun isGestureNavigationEnabled(contentResolver: ContentResolver): Boolean {
-        return Settings.Secure.getInt(contentResolver, "navigation_mode", 0) == 2
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun configureFullScreenMode() {
+        // Extends the PhotoView in the whole screen
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        // Hides StatusBar and Navigation bar, you have to tap to appear
+        // window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+        // Fixes the Full Screen black bar in screens with notch
+        window.attributes.layoutInDisplayCutoutMode =
+            WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+    }
+
+    private fun isGestureNavigationEnabled(): Boolean {
+        // Default navigation mode value
+        val defaultNavigationMode = 0
+        val gestureNavigationMode = 2
+
+        return Settings.Secure.getInt(contentResolver, "navigation_mode", defaultNavigationMode) == gestureNavigationMode
     }
 }
-
-
--- app/src/main/java/com/example/edgetoedgeexample/activitywithrecyclerview/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/edgetoedgeexample/activitywithrecyclerview/RecyclerViewActivity.kt
@@ -16,8 +16,8 @@ import com.example.edgetoedgeexample.R
 
 class RecyclerViewActivity : AppCompatActivity() {
 
-    var itemsArray = ArrayList<String>()
-    lateinit var adapter: RVAdapter
+    private var itemsArray = ArrayList<String>()
+    private lateinit var adapter: RVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,47 +26,54 @@ class RecyclerViewActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         val recyclerViewContentId = findViewById<RelativeLayout>(R.id.recycler_view_content_id)
 
-        // Setting layout and divider for the RecyclerView
+        initRecyclerView(recyclerView)
+        loadData()
+        adapter = RVAdapter(itemsArray)
+        recyclerView.adapter = adapter
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && isGestureNavigationEnabled()) {
+            extendBackgroundUnderNavigationBar(recyclerView, recyclerViewContentId)
+        }
+    }
+
+    private fun initRecyclerView(recyclerView: RecyclerView) {
         val llm = LinearLayoutManager(this)
         recyclerView.layoutManager = llm
         recyclerView.setHasFixedSize(true)
         val dividerItemDecoration = DividerItemDecoration(recyclerView.context, llm.orientation)
         recyclerView.addItemDecoration(dividerItemDecoration)
+    }
 
-        // Get the data and update the RecyclerView
-        loadData()
-        adapter = RVAdapter(itemsArray)
-        recyclerView.adapter = adapter
+    private fun extendBackgroundUnderNavigationBar(
+        recyclerView: RecyclerView,
+        recyclerViewContentId: RelativeLayout
+    ) {
+        // Disable clipping of the RecyclerView content when we use padding
+        recyclerView.clipToPadding = false
 
+        // Make the Gesture Navigation Bar transparent
+        window.navigationBarColor = Color.TRANSPARENT
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (isGestureNavigationEnabled(this.contentResolver)) {
-                // Disable clipping of the RecyclerView content when we use padding
-                recyclerView.clipToPadding = false
+        // Expand the Views (RecyclerView) under the gesture navigation bar and toolbar
+        window.decorView.systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
 
-                // Make the Gesture Navigation Bar transparent
-                window.navigationBarColor = Color.TRANSPARENT
-
-                // Expand the Views (RecyclerView) under the gesture navigation bar and toolbar
-                window.decorView.systemUiVisibility =
-                    (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
-
-                // Set padding for the Views (RecyclerView and Relative Layout) from the System Views (Gesture Navigation Bar, Toolbar)
-                recyclerViewContentId.setOnApplyWindowInsetsListener { v, insets ->
-                    val topPadding = insets.systemWindowInsetTop
-                    val bottomPadding = insets.systemWindowInsetBottom
-                    recyclerViewContentId.setPadding(0, topPadding, 0, 0)
-                    recyclerView.setPadding(0, 0, 0, bottomPadding)
-                    insets.consumeSystemWindowInsets()
-                }
-            }
+        // Set padding for the Views (RecyclerView and Relative Layout) from the System Views (Gesture Navigation Bar, Toolbar)
+        recyclerViewContentId.setOnApplyWindowInsetsListener { _, insets ->
+            val topPadding = insets.systemWindowInsetTop
+            val bottomPadding = insets.systemWindowInsetBottom
+            recyclerViewContentId.setPadding(0, topPadding, 0, 0)
+            recyclerView.setPadding(0, 0, 0, bottomPadding)
+            insets.consumeSystemWindowInsets()
         }
     }
 
-    private fun isGestureNavigationEnabled(contentResolver: ContentResolver): Boolean {
-        return Settings.Secure.getInt(contentResolver, "navigation_mode", 0) == 2
-    }
+    private fun isGestureNavigationEnabled(): Boolean {
+        val defaultNavigationMode = 0
+        val gestureNavigationMode = 2
 
+        return Settings.Secure.getInt(contentResolver, "navigation_mode", defaultNavigationMode) == gestureNavigationMode
+    }
 
     private fun loadData() {
         for (i in 0..20) {
